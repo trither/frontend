3e1b7093265b987c157b3533815f2d36564c20e5
rither <rither@pdx.edu>
2020-08-09 07:40:06 -0700

    redid functionality of checkboxes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,31 +4,37 @@ import  CheckBox  from './Checkbox'
 import Charts from './Charts'
 
 class App extends Component {
+  /**
+	 * Constructor that creates the state that will hold the data to be passed to Charts
+	 */
   constructor(props) {
     super(props);
     this.state = {
-      locationInputs: [
-        { groupId: 1, id: 1, coordinates: "&latitude=43.6415&longitude=-70.2409", value: "south", isChecked: false },
-        { groupId: 1, id: 2, coordinates: "&latitude=45.4475&longitude=-122.7221", value: "west", isChecked: false },
-        { groupId: 1, id: 3, coordinates: "&latitude=45.5154&longitude=-122.6604", value: "east", isChecked: false },
-        { groupId: 1, id: 4, coordinates: "&latitude=45.6075&longitude=-122.7236", value: "north", isChecked: false },
-      ],
       foodInputs: [
-        { groupId: 2, id: 1, coordinates: false, value: "vegan", isChecked: false },
-        { groupId: 2, id: 2, coordinates: false, value: "seafood", isChecked: false },
-        { groupId: 2, id: 3, coordinates: false, value: "burger", isChecked: false },
-        { groupId: 2, id: 4, coordinates: false, value: "vegetarian", isChecked: false },
+        { groupId: 1, id: 1, coordinates: false, value: "vegan", isChecked: false },
+        { groupId: 1, id: 2, coordinates: false, value: "seafood", isChecked: false },
+        { groupId: 1, id: 3, coordinates: false, value: "burger", isChecked: false },
+        { groupId: 1, id: 4, coordinates: false, value: "vegetarian", isChecked: false },
+      ],
+      locationInputs: [
+        { groupId: 2, id: 1, coordinates: "&latitude=43.6415&longitude=-70.2409", value: "south", isChecked: false },
+        { groupId: 2, id: 2, coordinates: "&latitude=45.4475&longitude=-122.7221", value: "west", isChecked: false },
+        { groupId: 2, id: 3, coordinates: "&latitude=45.5154&longitude=-122.6604", value: "east", isChecked: false },
+        { groupId: 2, id: 4, coordinates: "&latitude=45.6075&longitude=-122.7236", value: "north", isChecked: false },
       ]
 
     };
   }
 
-  handleAllChecked = id => event => {
+  /**
+	 * Clear all inputs and reset
+	 */
+  handleReset = id => event => {
     let locationInputs = this.state.locationInputs;
     locationInputs
       .filter(f => f.groupId === id)
       .forEach(searchInput => {
-        searchInput.isChecked = event.target.checked;
+        searchInput.isChecked = false;
       });
     this.setState({ locationInputs: locationInputs });
 
@@ -36,26 +42,38 @@ class App extends Component {
     foodInputs
       .filter(f => f.groupId === id)
       .forEach(searchInput => {
-        searchInput.isChecked = event.target.checked;
+        searchInput.isChecked = false;
       });
     this.setState({ foodInputs: foodInputs });
   };
 
-  handleCheckfieldElement = event => {
+  /**
+	 * Handles if a location was checked or not
+	 */
+  handleLocationCheckfieldElement = event => {
     let locationInputs = this.state.locationInputs;
     locationInputs.forEach(searchInput => {
       if (`${searchInput.groupId}-${searchInput.id}` === event.target.value)
         searchInput.isChecked = event.target.checked;
     });
     this.setState({ locationInputs: locationInputs });
+  };
 
+  /**
+	 * Handles if food category was checked or not
+	 */
+  handleFoodCheckfieldElement = event => {
     let foodInputs = this.state.foodInputs;
-    console.log("testing"+this.state.foodInputs[0].isChecked)
+    //console.log("testing"+this.state.foodInputs[0].isChecked)
     foodInputs.forEach(searchInput => {
       if (`${searchInput.groupId}-${searchInput.id}` === event.target.value)
         searchInput.isChecked = event.target.checked;
+      else{
+        searchInput.isChecked = false;
+      }
     });
     this.setState({ foodInputs: foodInputs });
+    
   };
 /*
   function createCharts(){
@@ -71,49 +89,46 @@ class App extends Component {
   }
 */
   render() {
-    //console.log(this.state.foodInputs);
     return (
       <div className="App">
         <h1> Homescreen description and welcome message goes here!  </h1>
         <h2> Choose food categories and areas of the portland area for restaurant info </h2>
-        {[{ id: 1, name: "Check / Uncheck All" }, { id: 2, name: "Check / Uncheck All" }].map(item => (
+        {[{ id: 1, name: "Reset" }, { id: 2, name: "Reset" }].map(item => (
           <div>
             <input
-              type="checkbox"
-              onChange={this.handleAllChecked(item.id)}
-              value="checkedall"
+              type="submit"
+              onClick={this.handleReset(item.id)}
+              //onChange={this.handleReset(item.id)}
+              value="Reset"
             />{" "}
-            {item.name}
             <ul>
-              {this.state.locationInputs
-                .filter(location => location.groupId === item.id)
-                .map((searchInput, index) => {
-                  return (
-                    <CheckBox
-                      key={`${item.id}-${searchInput.id}`}
-                      handleCheckfieldElement={this.handleCheckfieldElement}
-                      {...searchInput}
-                      value={`${item.id}-${searchInput.id}`}
-                      label={searchInput.value}/>
-
-                  );
-                })}
                 {this.state.foodInputs
                 .filter(food => food.groupId === item.id)
                 .map((searchInput, index) => {
                   return (
                     <CheckBox
                       key={`${item.id}-${searchInput.id}`}
-                      handleCheckfieldElement={this.handleCheckfieldElement}
+                      handleCheckfieldElement={this.handleFoodCheckfieldElement}
                       {...searchInput}
                       value={`${item.id}-${searchInput.id}`}
                       label={searchInput.value}/>
                   );
                 })}
+                {this.state.locationInputs
+                .filter(location => location.groupId === item.id)
+                .map((searchInput, index) => {
+                  return (
+                    <CheckBox
+                      key={`${item.id}-${searchInput.id}`}
+                      handleCheckfieldElement={this.handleLocationCheckfieldElement}
+                      {...searchInput}
+                      value={`${item.id}-${searchInput.id}`}
+                      label={searchInput.value}/>  
+                  );
+                })}
             </ul>
           </div>
         ))}
-        <Charts values={this.state.locationInputs}/>
       </div>
     );
   
